6bc71fe9963860808c4cfa859313b86ec46088d5
sasha11211 <kochinaleksandr@gmail.com>
2023-12-11 00:56:58 +0200

    fix bis req

--- src/main/java/com/selfman/business/requests/service/BusinessRequestsServiceImpl.java
+++ src/main/java/com/selfman/business/requests/service/BusinessRequestsServiceImpl.java
@@ -2,8 +2,10 @@ package com.selfman.business.requests.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
 import com.selfman.business.requests.dao.BusinessRequestsRepository;
 import com.selfman.business.requests.dto.BusinessRequestsCreateDto;
 import com.selfman.business.requests.dto.BusinessRequestsDto;
@@ -11,34 +13,41 @@ import com.selfman.business.requests.dto.CustomerBusinessRequestsDto;
 import com.selfman.business.requests.dto.ProviderBusinessRequestsDto;
 import com.selfman.business.requests.exceptions.BusinessRequestsNotFoundException;
 import com.selfman.business.requests.model.BusinessRequests;
-
+import com.selfman.provider.dao.ProviderRepository;
+import com.selfman.provider.exceptions.ProviderNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class BusinessRequestsServiceImpl implements BusinessRequestsService{
-	
+public class BusinessRequestsServiceImpl implements BusinessRequestsService {
+
 	final ModelMapper modelMapper;
 	final BusinessRequestsRepository businessRequestsRepository;
+	final ProviderRepository providerRepository;
 
 	@Override
 	public BusinessRequestsDto createBusinessRequests(BusinessRequestsCreateDto businessRequestsCreateDto) {
-		BusinessRequests businessRequests = modelMapper.map(businessRequestsCreateDto, BusinessRequests.class);
-        businessRequestsRepository.save(businessRequests);
-		return modelMapper.map(businessRequests, BusinessRequestsDto.class);
+		if (providerRepository.existsByEmail(businessRequestsCreateDto.getProviderEmail())) {
+			BusinessRequests businessRequests = modelMapper.map(businessRequestsCreateDto, BusinessRequests.class);
+			businessRequestsRepository.save(businessRequests);
+			return modelMapper.map(businessRequests, BusinessRequestsDto.class);
+		}
+
+		throw new ProviderNotFoundException();
 	}
 
 	@Override
 	public BusinessRequestsDto changeStatusRequest(String id, String status) {
-		BusinessRequests businessRequests = businessRequestsRepository.findById(id).orElseThrow(BusinessRequestsNotFoundException::new);
+		BusinessRequests businessRequests = businessRequestsRepository.findById(id)
+				.orElseThrow(BusinessRequestsNotFoundException::new);
 		businessRequests.setStatus(status);
 		businessRequestsRepository.save(businessRequests);
 		return modelMapper.map(businessRequests, BusinessRequestsDto.class);
 	}
 
 	@Override
-	public  List<CustomerBusinessRequestsDto> getCustomerBusinessRequests(String customerEmail) {
+	public List<CustomerBusinessRequestsDto> getCustomerBusinessRequests(String customerEmail) {
 		return businessRequestsRepository.findBusinessRequestsByCustomerEmail(customerEmail).stream()
 				.map(b -> modelMapper.map(b, CustomerBusinessRequestsDto.class)).collect(Collectors.toList());
 	}
@@ -51,7 +60,8 @@ public class BusinessRequestsServiceImpl implements BusinessRequestsService{
 
 	@Override
 	public BusinessRequestsDto getByIdBusinessRequests(String id) {
-		BusinessRequests businessRequests = businessRequestsRepository.findById(id).orElseThrow(BusinessRequestsNotFoundException::new);
+		BusinessRequests businessRequests = businessRequestsRepository.findById(id)
+				.orElseThrow(BusinessRequestsNotFoundException::new);
 		return modelMapper.map(businessRequests, BusinessRequestsDto.class);
 	}
 
